4cb6a457804779f32c150dac68e7728bc7f37626
bunde-uji <bundeuji@gmail.com>
2025-10-16 14:44:51 +0100

    refactoring on search page and hook

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,29 +1,91 @@
-import { Text, View } from 'react-native'
-import React, { useState } from 'react'
-import useFetch from '@/services/useFetch'
-import { fetchMovies } from '@/services/api';
-import SearchBar from '@/components/SearchBar';
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import SearchBar from "@/components/SearchBar";
+import MovieCard from "@/components/MovieCard";
 
 const Search = () => {
-  const { data, loading, error, refetch } = useFetch(() => fetchMovies({ query: "" }));
+  const { data, loading, error, refetch, reset } = useFetch(() =>
+    fetchMovies({ query: search }), false
+  );
   const [search, setSearch] = useState("");
 
+  useEffect(() => {
+    const setTimeoutId = setTimeout(() => {
+      if (search.trim()) {
+        refetch();
+      } else {
+        reset();
+      }
+    }, 1000);
+    return () => clearTimeout(setTimeoutId);
+  }, [search]);
+
   return (
-    <View className='flex-1 bg-primary p-5 pt-10'>
-      <SearchBar
-        placeholder="Search for a movie"
-        // onPress={() => router.push("/search")}
-        onChangeText={(text) => setSearch(text)}
-        value={search}
-        onSubmitEditing={() => {
-          setSearch(search);
-          refetch();
-        }}
-      />
+    <View className="flex-1 bg-primary p-5 pt-10">
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <MovieCard
+              id={item.id}
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+              release_date={item.release_date}
+            />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={3}
+          columnWrapperStyle={{
+            gap: 10,
+            justifyContent: "flex-start",
+          }}
+          scrollEnabled={true}
+          ListHeaderComponentStyle={{
+            marginBottom: 10,
+            position: "sticky"
+          }}
+          ListHeaderComponent={
+            <>
+              <SearchBar
+                placeholder="Search for a movie"
+                onChangeText={(text) => setSearch(text)}
+                value={search}
+                onSubmitEditing={() => {
+                  setSearch(search);
+                  console.log(search, "search");
+                  refetch();
+                }}
+              />
 
-      {data && <Text className="text-white text-2xl mb-5 font-bold">Search Results: {data.length}</Text>}
+              {/* {data && (
+                <Text className="text-white text-lg mb-5 font-bold">
+                  Search Results for &quot;{search}&quot;: ({data.length})
+                </Text>
+              )} */}
+
+              {loading ? (
+                <ActivityIndicator size="large" color="#1873c2" />
+              ) : error ? (
+                <Text className="text-white text-lg mb-5 font-bold">
+                  Error: {error.message}
+                </Text>
+              ) : data && data.length < 1 ? (
+                <Text className="text-white text-lg mb-5 font-bold">
+                  No results found
+                </Text>
+              ) : data && data.length > 0 ? (
+                <Text className="text-white text-lg mb-5 font-bold">
+                  Search Results for &quot;{search}&quot;: ({data.length})
+                </Text>
+              ) : null
+              }
+            </>
+          }
+        />
     </View>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- services/api.tsx
+++ services/api.tsx
@@ -8,20 +8,20 @@ export const TMDB_CONFIG = {
 }   
 
 export const fetchMovies = async ({ query }: { query: string }) => {
-    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/discover/movie?query=${query}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
+    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
 
     const response = await fetch(endpoint, {
         method: "GET",
         headers: TMDB_CONFIG.headers,
     })
 
-    // console.log(response.json());
-
-    console.log(response.status)
+    console.log(response, "response")
 
     if (!response.ok) {
         throw new Error("Failed to fetch movies")
     }
+
+    console.log(endpoint, "endpoint")
 
     const data = await response.json();
 
