288328300656293253857c991208b7b5f154697f
bunde-uji <bundeuji@gmail.com>
2025-10-26 16:36:26 +0100

    render title and poster image on movie details page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,34 +1,49 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import { useLocalSearchParams, useRouter } from 'expo-router'
-import { Ionicons } from '@expo/vector-icons'
-import useFetch from '@/services/useFetch'
-import { fetchMovieDetails } from '@/services/api'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Image
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
 
 const MovieDetails = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   const { id } = useLocalSearchParams();
 
-  const { data: movie, loading } = useFetch(() => fetchMovieDetails(id as string))
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
 
   return (
-    <View className='p-10 bg-primary flex-1'>
-      <View className='flex-row'>
-        <TouchableOpacity
-          onPress={() => router.back()}
-        >
-          <Ionicons
-            name='arrow-back'
-            size={20}
-            color="white"
+    <SafeAreaView className="bg-primary flex-1 w-full">
+      {movie && 
+        <ScrollView className="flex-row w-full">
+        <View className="flex-row items-center gap-2 p-5">
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={20} color="white" />
+          </TouchableOpacity>
+          <Text className="text-white text-lg font-medium">{movie && movie.original_title}</Text>
+        </View>
+        <View className="w-full">
+          <Image
+            source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+            className="h-96 w-screen"
+            resizeMode="stretch"
           />
-        </TouchableOpacity>
-        <Text>{movie && movie.original_title}</Text>
-      </View>
-    </View>
-  )
-}
+        </View>
+      </ScrollView>
+      }
+    </SafeAreaView>
+  );
+};
 
-export default MovieDetails
+export default MovieDetails;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
