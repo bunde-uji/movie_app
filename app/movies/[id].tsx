db53c675fb5c8e87d4f0a85c1da26c0f28aca798
bunde-uji <bundeuji@gmail.com>
2025-10-27 17:23:57 +0100

    overview and avergae movie ratings

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -21,12 +21,10 @@ const MovieDetails = () => {
     fetchMovieDetails(id as string)
   );
 
-  console.log(movie)
-
   return (
     <SafeAreaView className="bg-primary flex-1 w-full">
       {movie && 
-        <ScrollView className="flex-row w-full">
+        <ScrollView className="flex-col w-full" horizontal={false}>
         <View className="flex-row items-center gap-2 p-3">
           <TouchableOpacity onPress={() => router.back()}>
             <Ionicons name="arrow-back" size={25} color="white" />
@@ -45,7 +43,15 @@ const MovieDetails = () => {
           <View className="flex-row gap-2 items-center">
             <Text className="text-gray-300">{movie.release_date.split("-")[0]}</Text>
             <Text className="text-gray-300 px-1 py-[2px] bg-gray-700 text-sm rounded-sm">{movie.adult ? "18+" : "13+"}</Text>
+            <View className="flex-row items-center gap-1 text-gray-300 px-1 py-[2px] bg-gray-700 text-sm rounded-sm">
+              <Ionicons name="star" size={10} color="gold" />
+              <Text className="text-sm text-gray-300">{Math.round(movie.vote_average)}/10</Text>
+            </View>
             {/* <Text>{movie.}</Text> */}
+          </View>
+
+          <View className="flex-col">
+            <Text className="text-white pt-5 flex-wrap">{movie.overview}</Text>
           </View>
         </View>
       </ScrollView>
