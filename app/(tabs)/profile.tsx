cca2af5e335f0d63d183b5f23df8914ae9e05798
bunde-uji <bundeuji@gmail.com>
2025-11-07 23:07:36 +0100

    profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Search from "./search";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary flex-1">
+      <View className="p-3">
+        <Ionicons name="person" size={40} color="white" />
+        <Text className="text-white">Profile</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
