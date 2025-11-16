3689ece52067603e3d2a038ec09735f87f1d5fa2
bunde-uji <bundeuji@gmail.com>
2025-11-16 23:45:26 +0100

    remove unused variables

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,12 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Search from "./search";
 
 const Profile = () => {
   return (
     <SafeAreaView className="bg-primary flex-1">
-      <View className="p-5">
+      <View className="p-4">
         <Ionicons name="person" size={35} color="white" />
         <Text className="text-white">Profile</Text>
       </View>
